1fc609420d90817da17cd7903c683c4b4c8ef1f3
mizuno <31834@toyota.kosen-ac.jp>
2022-03-21 23:39:56 +0900

    Add: ability to get various music

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -4,14 +4,20 @@ class MusicsController < ApplicationController
     end
 
     def search
-        if params[:search].present?
+        if params[:search1].present?
             @searchartists = RSpotify::Artist.search(params[:search])
         end
     end
 
     def input
-        if params[:input].present?
-            @searchartist = RSpotify::Artist.search(params[:input]).first
+        if (params[:input1].present?) && (params[:input2].present?) && (params[:input3].present?)
+            searchartist1 = RSpotify::Artist.search(params[:input1]).first
+            searchartist2 = RSpotify::Artist.search(params[:input2]).first
+            searchartist3 = RSpotify::Artist.search(params[:input3]).first
+            genres = [searchartist1.genres, searchartist2.genres, searchartist3.genres]
+            genres = genres.flatten!
+            @genres = genres.sample(5)
+            @recommendations = RSpotify::Recommendations.generate(seed_genres: @genres)
         end
     end
 end
