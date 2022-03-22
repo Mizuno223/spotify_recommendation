82a452390b6b26421dd11d8dc2752a77b5068b3f
mizuno <31834@toyota.kosen-ac.jp>
2022-03-22 17:00:12 +0900

    Add: Getting reccomend musics

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -11,12 +11,20 @@ class MusicsController < ApplicationController
 
     def input
         if (params[:input1].present?) && (params[:input2].present?) && (params[:input3].present?)
-            searchartist1 = RSpotify::Artist.search(params[:input1]).first
-            searchartist2 = RSpotify::Artist.search(params[:input2]).first
-            searchartist3 = RSpotify::Artist.search(params[:input3]).first
-            genres = [searchartist1.genres, searchartist2.genres, searchartist3.genres]
-            genres = genres.flatten!
-            @genres = genres.sample(5)
+            @searchartist1 = RSpotify::Artist.search(params[:input1]).first
+            @searchartist2 = RSpotify::Artist.search(params[:input2]).first
+            @searchartist3 = RSpotify::Artist.search(params[:input3]).first
+
+            #重複してるジャンルを取り出す
+            duplicateGenre = [@searchartist1.genres, @searchartist2.genres, @searchartist3.genres].inject(&:&)
+            
+            #重複していないジャンルから合計5個になるようにランダムに取り出す
+            genres = [@searchartist1.genres, @searchartist2.genres, @searchartist3.genres].flatten!
+            counts = Hash.new(0)
+            genres.each { |v| counts[v] += 1 }
+            uniqueGenre = counts.select { |v, count| count == 1 }.keys
+
+            @genres = duplicateGenre.push(uniqueGenre.sample(5 - duplicateGenre.length)).flatten!
             @recommendations = RSpotify::Recommendations.generate(seed_genres: @genres)
         end
     end
